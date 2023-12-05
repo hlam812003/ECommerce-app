ccb9059f402faa22c74edb2ef0d61a2d6a20dffb
powoftech <powoftech@gmail.com>
2023-12-06 06:24:11 +0700

    Update pom.xml, persistence.xml. Create DBUtil

---
+++ ecommerceapp/src/main/java/com/data/DBUtil.java
@@ -0,0 +1,12 @@
+package com.data;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class DBUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("eCommercePU");
+
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
+}
