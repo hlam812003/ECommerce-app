70b3a8ca6711145a4e88de5cf4fd53b9bafdb861
HWAN <68631787+hlam812003@users.noreply.github.com>
2023-12-06 07:21:33 +0700

    Update

---
+++ ecommerceapp/src/main/java/data/UserDB.java
@@ -0,0 +1,53 @@
+package data;
+
+import jakarta.persistence.*;
+import model.User;
+import util.DBUtil;
+
+public class UserDB {
+    public static void insert(User user) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        try {
+            em.persist(user);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void update(User user) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();       
+        try {
+            em.merge(user);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void delete(User user) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();        
+        try {
+            em.remove(em.merge(user));
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        } finally {
+            em.close();
+        }       
+    }
+
+}
--- ecommerceapp/src/main/java/util/DBUtil.java
+++ ecommerceapp/src/main/java/util/DBUtil.java
@@ -1,4 +1,4 @@
-package com.data;
+package util;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
