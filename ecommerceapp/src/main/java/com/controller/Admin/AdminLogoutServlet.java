56009f04fc06e718e905eab47b8341cab31adfc8
HWAN <68631787+hlam812003@users.noreply.github.com>
2023-12-11 02:14:10 +0700

    Update Admin Logout

--- ecommerceapp/src/main/java/com/controller/Admin/AdminLogoutServlet.java
+++ ecommerceapp/src/main/java/com/controller/Admin/AdminLogoutServlet.java
@@ -1,5 +1,37 @@
 package com.controller.Admin;
 
-public class AdminLogoutServlet {
-    
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/admin/logout")
+public class AdminLogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        // Xóa session
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        // Xóa cookie
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("adminEmail".equals(cookie.getName()) || "adminPassword".equals(cookie.getName())) {
+                    cookie.setMaxAge(0);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+            }
+        }
+
+        // Chuyển hướng đến trang đăng nhập của quản trị viên
+        response.sendRedirect(request.getContextPath() + "/admin/login");
+    }
 }
