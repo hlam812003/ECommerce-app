7c7a56c2fade3a6c4f2e571bc2c3688083929b34
HWAN <68631787+hlam812003@users.noreply.github.com>
2023-12-11 10:59:40 +0700

    Update

--- ecommerceapp/src/main/java/com/mail/MailSendTSL.java
+++ ecommerceapp/src/main/java/com/mail/MailSendTSL.java
@@ -2,17 +2,24 @@ package com.mail;
 
 import java.util.Properties;
 
+import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
+import jakarta.activation.FileDataSource;
 import jakarta.mail.Authenticator;
+import jakarta.mail.BodyPart;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
 import jakarta.mail.PasswordAuthentication;
 import jakarta.mail.Session;
 import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 public class MailSendTSL {
-    public static void sendEmail(String recipientsEmail, String subject, String htmlContent) throws MessagingException {
+    public static void sendEmail(String recipientsEmail, String subject, String htmlContent, String filePath) throws MessagingException {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -33,11 +40,29 @@ public class MailSendTSL {
             message.setFrom(new InternetAddress(MailConfig.FROM));
             // set To email field
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientsEmail));
-            // set email subject field
+            // Setting the Subject and Content Type
             message.setSubject(subject);
-            // set the content of the email message
             message.setContent(htmlContent, "text/html");
-            // send the email message
+
+            // Create the message part with the HTML content
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(htmlContent, "text/html");
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is attachment
+            messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(filePath);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName("/view/invoice-email.jsp");
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+
+            // send the message
             Transport.send(message);
 
             System.out.println("Message sent successfully");
